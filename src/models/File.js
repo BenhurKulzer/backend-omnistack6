8f9dd1d1066bf3963c612e70a3cfa8da4ab5b5b0
Benhur Külzer <benhur.bitpc@gmail.com>
2019-04-15 23:15:26 -0300

    Ajustes finais Heroku

--- src/models/File.js
+++ src/models/File.js
@@ -19,7 +19,7 @@ const File = new mongoose.Schema(
 );
 
 File.virtual('url').get(function() {
-  const url = process.env.URL || 'http://localhost:3800'
+  const url = process.env.URL || 'http://localhost:3800';
 
   return `${url}/files/${encodeURIComponent(this.path)}`;
 })
